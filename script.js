d9fe4ccdeef60959ee1fa8c55638011d1d390f80
Chris <123cmorales@frhsd.com>
2023-02-15 08:27:23 -0500

    Viewed Directives

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const app = Vue.createApp({
     //template: '<h2>I am the ONE WHO KNOCKS</h2>'
     data(){
         return {
+            showBooks: true,
             title: 'I am Heisenberg',
             author: 'Walter Heartwell White',
             age: 52
         }
     },
     methods: {
-        
+        toggleShowBooks () {
+            this.showBooks = !this.showBooks
+        }
+        //changeTitle(abc) {
+          // this.title = 'Los Pollos Hermanos'
+          //this.title = abc;
+        //}
     }
 });
 
