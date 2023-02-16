eacf377f5581c127ed2fe41281145cf6c548c96b
Chris <123cmorales@frhsd.com>
2023-02-16 09:46:46 -0500

    Starting binding

--- script.js
+++ script.js
@@ -3,28 +3,34 @@ const app = Vue.createApp({
     //template: '<h2>I am the ONE WHO KNOCKS</h2>'
     data(){
         return {
+            url: 'https://netninja.dev/',
             showBooks: true,
-            title: 'I am Heisenberg',
-            author: 'Walter Heartwell White',
-            age: 52,
-            x: 0,
-            y: 0
+            books: [
+                {title: 'wow', author: 'bobby'},
+                {title: 'eric', author: 'Cartman'},
+                {title: 'Jimmy', author: 'Timmah'},
+            ]
+            // title: 'I am Heisenberg',
+            // author: 'Walter Heartwell White',
+            // age: 52,
+            // x: 0,
+            // y: 0
         }
     },
     methods: {
         toggleShowBooks () {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data) {
-            console.log(e, e.type)
-            if (data) {
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
+        // handleEvent(e, data) {
+        //     console.log(e, e.type)
+        //     if (data) {
+        //         console.log(data)
+        //     }
+        // },
+        // handleMousemove(e) {
+        //     this.x = e.offsetX
+        //     this.y = e.offsetY
+        // }
         //changeTitle(abc) {
           // this.title = 'Los Pollos Hermanos'
           //this.title = abc;
