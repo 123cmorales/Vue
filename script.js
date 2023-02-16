644890ede89c6ba77b38f7f92fae66696c0bac3c
Chris <123cmorales@frhsd.com>
2023-02-16 09:24:53 -0500

    Mouse events

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'I am Heisenberg',
             author: 'Walter Heartwell White',
-            age: 52
+            age: 52,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks () {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
         //changeTitle(abc) {
           // this.title = 'Los Pollos Hermanos'
